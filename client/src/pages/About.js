37e0d13ec8214fe7dea37259516dd90269764725
Duc Bach <bachtrananhduc@gmail.com>
2022-09-07 19:47:12 +0700

    init

---
+++ client/src/pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className="section-about">
+      <h1>About me</h1>
+    </section>
+  );
+};
+
+export default About;
---
+++ client/src/pages/index.js
@@ -0,0 +1,8 @@
+import Landing from './Landing';
+import Dashboard from './Dashboard';
+import Error from './Error';
+import Navbar from './Navbar';
+import Register from './Register';
+import ShareLayout from './ShareLayout';
+
+export { Landing, Dashboard, Error, Navbar, Register, ShareLayout };
