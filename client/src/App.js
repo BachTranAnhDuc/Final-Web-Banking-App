bfd21d5c64e2c848b30fee82c5af4d23b75965f4
Duc Bach <bachtrananhduc@gmail.com>
2022-09-17 20:51:54 +0700

    error page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { About, Landing, Register, Login } from './pages';
+import { About, Landing, Register, Login, Error } from './pages';
 import { ShareLayout } from './components';
 
 const App = () => {
@@ -12,6 +12,7 @@ const App = () => {
           <Route path="/about" element={<About></About>}></Route>
           <Route path="/register" element={<Register></Register>}></Route>
           <Route path="/login" element={<Login></Login>}></Route>
+          <Route path="*" element={<Error></Error>}></Route>
         </Route>
       </Routes>
     </BrowserRouter>
--- client/src/pages/Error.js
+++ client/src/pages/Error.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import ErrorImage from '../assets/images/error.svg';
 
 const Error = () => {
-  return <div>Error</div>;
+  return (
+    <section className="section-error">
+      <img src={ErrorImage} alt="error image" className="error-image" />
+      <h2 className="heading--secondary">Something went wrong!</h2>
+    </section>
+  );
 };
 
 export default Error;
