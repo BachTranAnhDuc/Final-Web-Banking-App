077f568c993665de1077a1f36e1c1b5092a9ba2f
Duc Bach <bachtrananhduc@gmail.com>
2022-10-06 21:38:36 +0700

    buy card raw

--- client/src/pages/bank/BuyCard.js
+++ client/src/pages/bank/BuyCard.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 
 import { HiOutlinePlusCircle, HiOutlineMinusCircle } from 'react-icons/hi';
 
+import viettelLogo from '../../assets/images/logos/viettel.svg';
+import successSvg from '../../assets/images/design/success.svg';
+
 const initSwitchCard = {
   viettel: true,
   mobile: false,
@@ -20,9 +23,9 @@ const BuyCard = () => {
 
   const [valuesItem, setValuesItem] = useState(initItem);
 
-  const [styleItem, setStyleItem] = useState(
-    'buy-card__item-card buy-card__item-card--viettel buy-card__item-card--viettel-active'
-  );
+  const [getAmount, setAmount] = useState(1);
+
+  const [getPage, setPage] = useState(1);
 
   const resetSwitchCard = () => {
     setValuesCard({ viettel: false, mobile: false, vina: false });
@@ -44,121 +47,330 @@ const BuyCard = () => {
     setValuesCard({ viettel: false, mobile: false, [e.target.name]: true });
   };
 
+  const handleClick10 = () => {
+    setValuesItem({ ten: true, twenty: false, fifty: false, hundred: false });
+  };
+  const handleClick20 = () => {
+    setValuesItem({ ten: false, twenty: true, fifty: false, hundred: false });
+  };
+  const handleClick50 = () => {
+    setValuesItem({ ten: false, twenty: false, fifty: true, hundred: false });
+  };
+  const handleClick100 = () => {
+    setValuesItem({ ten: false, twenty: false, fifty: false, hundred: true });
+  };
+
+  const handleClickMinus = (e) => {
+    if (getAmount === 1) {
+      setAmount(1);
+    } else {
+      setAmount(getAmount - 1);
+    }
+  };
+
+  const handleClickPlus = (e) => {
+    if (getAmount === 5) {
+      setAmount(5);
+    } else {
+      setAmount(getAmount + 1);
+    }
+  };
+
+  const handleClickNextPage = (e) => {
+    if (getPage === 3) {
+      setPage(3);
+    } else {
+      setPage(getPage + 1);
+    }
+  };
+  const handleClickPrePage = (e) => {
+    if (getPage === 1) {
+      setPage(1);
+    } else {
+      setPage(getPage - 1);
+    }
+  };
+
   return (
     <div className="bank-body__buy-card">
       <div className="buy-card__header">
         <h2 className="buy-card__header--heading">Buy card</h2>
       </div>
       <div className="buy-card__body">
-        <div className="buy-card__body--top">
-          <div className="buy-card__list">
-            <button
-              // className="buy-card__item-card buy-card__item-card--viettel"
-              className={
-                valuesCard.viettel
-                  ? 'buy-card__item-card buy-card__item-card--viettel buy-card__item-card--viettel-active'
-                  : 'buy-card__item-card buy-card__item-card--viettel'
-              }
-              onClick={handleSwitchViettel}
-              name="viettel"
-            >
-              Viettel
-            </button>
-            <button
-              className={
-                valuesCard.mobile
-                  ? 'buy-card__item-card buy-card__item-card--mobile buy-card__item-card--mobile-active'
-                  : 'buy-card__item-card buy-card__item-card--mobile'
-              }
-              onClick={handleSwitchMobile}
-              name="mobile"
-            >
-              Mobilephone
-            </button>
-            <button
-              className={
-                valuesCard.vina
-                  ? 'buy-card__item-card buy-card__item-card--vinaphone buy-card__item-card--vinaphone-active'
-                  : 'buy-card__item-card buy-card__item-card--vinaphone'
-              }
-              onClick={handleSwitchVina}
-              name="vina"
-            >
-              Vinaphone
-            </button>
-          </div>
-        </div>
-        <div className="buy-card__body--bottom">
-          <div className="buy-card__list--items">
-            <button
-              className={
-                valuesCard.mobile
-                  ? 'buy-card__item-value buy-card__item--mobile buy-card__item--mobile--active'
-                  : valuesCard.vina
-                  ? 'buy-card__item-value buy-card__item--vinaphone buy-card__item--vinaphone--active'
-                  : 'buy-card__item-value buy-card__item--viettel buy-card__item--viettel--active'
-              }
-            >
-              10000vnd
-            </button>
-            <button
-              className={
-                valuesCard.mobile
-                  ? 'buy-card__item-value buy-card__item--mobile buy-card__item--mobile--active'
-                  : valuesCard.vina
-                  ? 'buy-card__item-value buy-card__item--vinaphone buy-card__item--vinaphone--active'
-                  : 'buy-card__item-value buy-card__item--viettel buy-card__item--viettel--active'
-              }
-            >
-              20000vnd
-            </button>
-            <button
-              className={
-                valuesCard.mobile
-                  ? 'buy-card__item-value buy-card__item--mobile buy-card__item--mobile--active'
-                  : valuesCard.vina
-                  ? 'buy-card__item-value buy-card__item--vinaphone buy-card__item--vinaphone--active'
-                  : 'buy-card__item-value buy-card__item--viettel buy-card__item--viettel--active'
-              }
-            >
-              50000vnd
-            </button>
-            <button
-              className={
-                valuesCard.mobile
-                  ? 'buy-card__item-value buy-card__item--mobile buy-card__item--mobile--active'
-                  : valuesCard.vina
-                  ? 'buy-card__item-value buy-card__item--vinaphone buy-card__item--vinaphone--active'
-                  : 'buy-card__item-value buy-card__item--viettel buy-card__item--viettel--active'
-              }
-            >
-              100000vnd
-            </button>
-          </div>
-
-          <div className="buy-card__item-number">
-            <div className="buy-card__item-number--left">
-              <span className="buy-card__item--text">Number</span>
-              <button className="btn__buy-card--action">
-                <HiOutlineMinusCircle className="buy-card__icon"></HiOutlineMinusCircle>
-              </button>
-              <span className="buy-card__item--span">1</span>
-              <button className="btn__buy-card--action">
-                <HiOutlinePlusCircle className="buy-card__icon"></HiOutlinePlusCircle>
-              </button>
-            </div>
-            <div className="buy-card__item-number--right">
-              <div className="buy-card__group-btns">
-                <button className="buy-card__btn" type="button">
-                  Back
+        {getPage === 1 && (
+          <>
+            <div className="buy-card__body--top">
+              <div className="buy-card__list">
+                <button
+                  // className="buy-card__item-card buy-card__item-card--viettel"
+                  className={
+                    valuesCard.viettel
+                      ? 'buy-card__item-card buy-card__item-card--viettel buy-card__item-card--viettel-active'
+                      : 'buy-card__item-card buy-card__item-card--viettel'
+                  }
+                  onClick={handleSwitchViettel}
+                  name="viettel"
+                >
+                  Viettel
                 </button>
-                <button className="buy-card__btn" type="button">
-                  Back
+                <button
+                  className={
+                    valuesCard.mobile
+                      ? 'buy-card__item-card buy-card__item-card--mobile buy-card__item-card--mobile-active'
+                      : 'buy-card__item-card buy-card__item-card--mobile'
+                  }
+                  onClick={handleSwitchMobile}
+                  name="mobile"
+                >
+                  Mobilephone
+                </button>
+                <button
+                  className={
+                    valuesCard.vina
+                      ? 'buy-card__item-card buy-card__item-card--vinaphone buy-card__item-card--vinaphone-active'
+                      : 'buy-card__item-card buy-card__item-card--vinaphone'
+                  }
+                  onClick={handleSwitchVina}
+                  name="vina"
+                >
+                  Vinaphone
                 </button>
               </div>
             </div>
-          </div>
-        </div>
+            <div className="buy-card__body--bottom">
+              <div className="buy-card__list--items">
+                <button
+                  className={
+                    valuesCard.mobile
+                      ? `buy-card__item-value buy-card__item--mobile buy-card__item--mobile--active ${
+                          valuesItem.ten && 'buy-card__item--mobile-active'
+                        }`
+                      : valuesCard.vina
+                      ? `buy-card__item-value buy-card__item--vinaphone buy-card__item--vinaphone--active ${
+                          valuesItem.ten && 'buy-card__item--vinaphone-active'
+                        }`
+                      : `buy-card__item-value buy-card__item--viettel buy-card__item--viettel--active ${
+                          valuesItem.ten && 'buy-card__item--viettel-active'
+                        }`
+                  }
+                  onClick={handleClick10}
+                >
+                  10000vnd
+                </button>
+                <button
+                  className={
+                    valuesCard.mobile
+                      ? `buy-card__item-value buy-card__item--mobile buy-card__item--mobile--active ${
+                          valuesItem.twenty && 'buy-card__item--mobile-active'
+                        }`
+                      : valuesCard.vina
+                      ? `buy-card__item-value buy-card__item--vinaphone buy-card__item--vinaphone--active ${
+                          valuesItem.twenty &&
+                          'buy-card__item--vinaphone-active'
+                        }`
+                      : `buy-card__item-value buy-card__item--viettel buy-card__item--viettel--active ${
+                          valuesItem.twenty && 'buy-card__item--viettel-active'
+                        }`
+                  }
+                  onClick={handleClick20}
+                >
+                  20000vnd
+                </button>
+                <button
+                  className={
+                    valuesCard.mobile
+                      ? `buy-card__item-value buy-card__item--mobile buy-card__item--mobile--active ${
+                          valuesItem.fifty && 'buy-card__item--mobile-active'
+                        }`
+                      : valuesCard.vina
+                      ? `buy-card__item-value buy-card__item--vinaphone buy-card__item--vinaphone--active ${
+                          valuesItem.fifty && 'buy-card__item--vinaphone-active'
+                        }`
+                      : `buy-card__item-value buy-card__item--viettel buy-card__item--viettel--active ${
+                          valuesItem.fifty && 'buy-card__item--viettel-active'
+                        }`
+                  }
+                  onClick={handleClick50}
+                >
+                  50000vnd
+                </button>
+                <button
+                  className={
+                    valuesCard.mobile
+                      ? `buy-card__item-value buy-card__item--mobile buy-card__item--mobile--active ${
+                          valuesItem.hundred && 'buy-card__item--mobile-active'
+                        }`
+                      : valuesCard.vina
+                      ? `buy-card__item-value buy-card__item--vinaphone buy-card__item--vinaphone--active ${
+                          valuesItem.hundred &&
+                          'buy-card__item--vinaphone-active'
+                        }`
+                      : `buy-card__item-value buy-card__item--viettel buy-card__item--viettel--active ${
+                          valuesItem.hundred && 'buy-card__item--viettel-active'
+                        }`
+                  }
+                  onClick={handleClick100}
+                >
+                  100000vnd
+                </button>
+              </div>
+
+              <div className="buy-card__item-number">
+                <div className="buy-card__item-number--left">
+                  <span className="buy-card__item--text">Number</span>
+                  <button
+                    className="btn__buy-card--action"
+                    type="button"
+                    onClick={handleClickMinus}
+                  >
+                    <HiOutlineMinusCircle className="buy-card__icon"></HiOutlineMinusCircle>
+                  </button>
+                  <span className="buy-card__item--span">{getAmount}</span>
+                  <button
+                    className="btn__buy-card--action"
+                    type="button"
+                    onClick={handleClickPlus}
+                  >
+                    <HiOutlinePlusCircle className="buy-card__icon"></HiOutlinePlusCircle>
+                  </button>
+                </div>
+                <div className="buy-card__item-number--right">
+                  <div className="buy-card__group-btns">
+                    <button
+                      className="buy-card__btn"
+                      type="button"
+                      onClick={handleClickPrePage}
+                    >
+                      Back
+                    </button>
+                    <button
+                      className="buy-card__btn"
+                      type="button"
+                      onClick={handleClickNextPage}
+                    >
+                      Next
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </>
+        )}
+
+        {getPage === 2 && (
+          <>
+            <div className="buy-card__shop--body-infor">
+              <div className="buy-card__shop--header">
+                <h2 className="buy-card__shop--heading">Infomation</h2>
+              </div>
+              <div className="buy-card__shop--body">
+                <div className="buy-card__shop--body__content">
+                  <div className="buy-card__shop--body__content--left">
+                    <img
+                      src={viettelLogo}
+                      alt="logo"
+                      className="buy-card__shop--body__content--img"
+                    />
+                  </div>
+                  <div className="buy-card__shop--body__content--right">
+                    <div className="buy-card__shop--body__content-control">
+                      <h2 className="buy-card__shop--body__content-heading">
+                        Type
+                      </h2>
+                      <span className="buy-card__shop--body__content-span">
+                        Viettel
+                      </span>
+                    </div>
+                    <div className="buy-card__shop--body__content-control">
+                      <h2 className="buy-card__shop--body__content-heading">
+                        Value
+                      </h2>
+                      <span className="buy-card__shop--body__content-span">
+                        100000vnd
+                      </span>
+                    </div>
+                    <div className="buy-card__shop--body__content-control">
+                      <h2 className="buy-card__shop--body__content-heading">
+                        Amount
+                      </h2>
+                      <span className="buy-card__shop--body__content-span">
+                        4
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="buy-card__shop--footer">
+              <div className="buy-card__shop--footer-btns">
+                <button
+                  className="buy-card__shop--footer-btn"
+                  type="button"
+                  onClick={handleClickPrePage}
+                >
+                  Back
+                </button>
+                <button
+                  className="buy-card__shop--footer-btn"
+                  type="button"
+                  onClick={handleClickNextPage}
+                >
+                  Next
+                </button>
+              </div>
+            </div>
+          </>
+        )}
+
+        {getPage === 3 && (
+          <>
+            <div className="buy-card__result">
+              <div className="buy-card__result--header">
+                <h2 className="buy-card__result--header__heading">Info</h2>
+              </div>
+
+              <div className="buy-card__result--body">
+                <div className="buy-card__result--body-left">
+                  <img
+                    className="buy-card__result--body-left__img"
+                    src={successSvg}
+                    alt="success"
+                  ></img>
+                </div>
+                <div className="buy-card__result--body-right">
+                  <div className="buy-card__result--body-right__control">
+                    <h2 className="buy-card__result--body-right__heading">
+                      Seri
+                    </h2>
+                    <span className="buy-card__result--body-right__span">
+                      123456789
+                    </span>
+                  </div>
+                </div>
+                <div className="buy-card__result--body-right"></div>
+              </div>
+            </div>
+
+            <div className="buy-card__result--footer">
+              <div className="buy-card__result--footer-btns">
+                <button
+                  className="buy-card__result--footer-btn"
+                  type="button"
+                  onClick={handleClickPrePage}
+                >
+                  Back
+                </button>
+                <button
+                  className="buy-card__result--footer-btn"
+                  type="button"
+                  onClick={handleClickNextPage}
+                >
+                  Next
+                </button>
+              </div>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
